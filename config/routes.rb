90fd573205c1ff0b84c71f0165350e5911e666cd
Harsh Trivedi <harshjtrivedi94@gmail.com>
2014-03-30 01:13:13 +0530

    Create links and repos controller and views. Implement appropiate actions in both configured with proper routing and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,60 +1,14 @@
 Pinlinks::Application.routes.draw do
   devise_for :users
 
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
+  get '/delete_repo/:repo_id', to: 'repos#destroy'  
+  get '/:repo_name/delete_link/:link_id', to: 'links#destroy'  
+  get '/:repo_name/new_links', to: 'links#new'  
+  get '/:user_name/:repo_name', to: 'repos#show'  
+  get '/create_links', to: 'links#create'  
+  get '/new_repo', to: 'repos#new'  
+  get '/:user_name', to: 'repos#list'  
+  root :to => 'repos#index'
 
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
-
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
-
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
+  match ':controller(/:action(/:id))(.:format)'
 end
