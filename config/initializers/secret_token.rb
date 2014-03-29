7c092554984ec58168553f82c8eff043db1afc9e
Shubham Patel <shubhpatel108@gmail.com>
2014-03-29 06:36:03 +0530

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pinlinks::Application.config.secret_token = 'c2a80f704bf8ea8df14d3d62c138bbcb28ce9f24e030d86e721ba61b269f9d77965681f2c5e0979b0b2e00f07015238715337c77d27396cb6be88ba978f827a6'
