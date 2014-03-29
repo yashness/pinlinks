4788f5b0cf15365df7eec1ffe570f0ec295fb6f1
Harsh Trivedi <harshjtrivedi94@gmail.com>
2014-03-30 01:04:05 +0530

    Add validates methods in links and repo model

--- app/models/link.rb
+++ app/models/link.rb
@@ -3,4 +3,6 @@ class Link < ActiveRecord::Base
 
   belongs_to :repo
   validates :repo_id, presence: true
+  validates :actual_link, presence: true, 
+  					  uniqueness: true
 end
--- app/models/repo.rb
+++ app/models/repo.rb
@@ -1,7 +1,9 @@
 class Repo < ActiveRecord::Base
-  attr_accessible :name , :tags
+  attr_accessible :name , :tags , :user_id
 
   belongs_to :user
   has_many :links
   validates :user_id, presence: true
+  validates :name, presence: true, 
+  					  uniqueness: true
 end
