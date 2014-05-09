76a7e72667c20b73eeef5384601250f33affbbd2
Harsh Trivedi <harshjtrivedi94@gmail.com>
2014-05-09 09:54:36 +0530

    split the send_links model method to send_pinlinks and send_manual_links.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,19 @@ class UserMailer < ActionMailer::Base
   default from: "admin@pinlinks.com"
 
 
-  def send_links(user_email , links, message)
+  #links should be array of Link objects
+  def send_pinlinks(user_email , links, message)
+  	@user_email = user_email 
+  	@links = links 
+  	@message = message
+
+  	if not @links.empty?
+		mail(to: @user_email, subject: "Some One sent you links using pinlinks")
+	end
+  end
+
+  #links should be array of strings
+  def send_manual_links(user_email , links, message)
   	@user_email = user_email 
   	@links = links 
   	@message = message
