430f0bae70ed39cfde061d11683e820834796a57
Shubham Patel <shubhpatel108@gmail.com>
2014-05-14 10:09:30 +0530

    Allow User to Login with Google Account using Omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
-  devise :omniauthable, :omniauth_providers => [:facebook]
+  devise :omniauthable, :omniauth_providers => [:facebook, :google_oauth2]
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :profile_name
   # attr_accessible :title, :body
@@ -30,6 +30,16 @@ class User < ActiveRecord::Base
       user.authid = auth.uid
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
+    end
+  end
+
+  def self.find_for_google_oauth2(auth)
+    where(auth.slice(:provider, :authid)).first_or_create do |user|
+      user.provider = auth.provider
+      user.authid = auth.uid
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.profile_name = user.email.split('@').first
     end
   end
 
